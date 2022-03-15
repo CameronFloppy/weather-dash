4e8e8acc6d2bdca598a645ddb4e05c7b8e0f111b
Jay Ford <jay.ford45@gmail.com>
2022-03-15 12:37:36 -0400

    added content to jumbotron

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ let cityLat = 0
 let cityInputEl = document.querySelector("#city");
 let cityFormEl = document.querySelector("#city-form");
 let forecastHeaderEl = document.querySelector("#forecast-header")
+let jumboEl = document.querySelector("#jumbotron");
 
 function getLonLat(event) {
     event.preventDefault();
@@ -37,24 +38,48 @@ function getForecast(cityLon, cityLat) {
 }
 
 function getCurrentWeather(data) {
-    let cityName = cityInputEl.value.trim();
-    let currentTemp = data.current.temp
-    let currentWind = data.current.wind_speed
-    let currentHumidity = data.current.humidity
-    let currentUV = data.current.uvi
-    console.log(cityName, currentTemp, currentWind, currentHumidity, currentUV)
+    // let cityName = cityInputEl.value.trim();
+    // let currentTemp = data.current.temp
+    // let currentWind = data.current.wind_speed
+    // let currentHumidity = data.current.humidity
+    // let currentUV = data.current.uvi
+    // console.log(cityName, currentTemp, currentWind, currentHumidity, currentUV)
+
+    let cityName = document.createElement("h3");
+    cityName.textContent = cityInputEl.value.trim();
+
+    let currentTemp = document.createElement("p");
+    currentTemp.textContent = data.current.temp;
+
+    let currentWind = document.createElement("p");
+    currentWind.textContent = data.current.wind_speed;
+
+    let currentHumidity = document.createElement("p");
+    currentHumidity.textContent = data.current.humidity;
+
+    let currentUV = document.createElement("p");
+    currentUV.textContent = data.current.uvi;
+    if (data.current.uvi <= 2) {
+        currentUV.setAttribute("class", "success")
+    } else if (data.current.uvi > 2 && data.current.uvi < 8) {
+        currentUV.setAttribute("class", "warning");
+    } else if (data.current.uvi === 8) {
+        currentUV.setAttribute("class", "danger");
+    }
+
+    jumboEl.append(cityName, currentTemp, currentWind, currentHumidity, currentUV);
+
 }
 
 function getDailyWeather(data) {
     let weatherCard = document.createElement("div");
-    dateEl.textContent = ""
-    dailyTemp.textContent = ""
-    dailyWind.textContent = ""
-    dailyHumid.textContent = ""
-
+    weatherCard.setAttribute("class", "justify-content-evenly row")
+    
 
     for (let i = 0; i < 5; i++) {
         let date = new Date((data.daily[i].dt*1000)-(data.timezone_offset*1000))
+        let dateCard = document.createElement("div")
+        dateCard.setAttribute("class", "card col-2")
         let dateEl = document.createElement("h4")
         console.log((date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear())
         dateEl.textContent = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear()
@@ -69,7 +94,8 @@ function getDailyWeather(data) {
         dailyHumid.textContent = data.daily[i].humidity
 
         console.log(dateEl, dailyTemp, dailyWind, dailyHumid)
-        weatherCard.append(dateEl, dailyTemp, dailyWind, dailyHumid)
+        dateCard.append(dateEl, dailyTemp, dailyWind, dailyHumid)
+        weatherCard.append(dateCard)
     }
     forecastHeaderEl.append(weatherCard);
 
