fdc2a5a00a5b5139aef40553a8fdbc3a0ea4b39b
Jay Ford <jay.ford45@gmail.com>
2022-03-15 18:58:18 -0400

    finished formatting forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,33 +38,35 @@ function getForecast(cityLon, cityLat) {
 }
 
 function getCurrentWeather(data) {
-    // let cityName = cityInputEl.value.trim();
-    // let currentTemp = data.current.temp
-    // let currentWind = data.current.wind_speed
-    // let currentHumidity = data.current.humidity
-    // let currentUV = data.current.uvi
-    // console.log(cityName, currentTemp, currentWind, currentHumidity, currentUV)
-
+    let date = new Date((data.current.dt*1000)-(data.timezone_offset*1000))
     let cityName = document.createElement("h3");
-    cityName.textContent = cityInputEl.value.trim();
+    cityName.textContent = cityInputEl.value.trim() + " " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+    let currentIconEl = document.createElement("img");
+    let icon = data.current.weather[0].icon
+    currentIconEl.setAttribute("src", "http://openweathermap.org/img/wn/"+ icon + "@2x.png");
+    currentIconEl.setAttribute("class", "weather-icon");
+    cityName.appendChild(currentIconEl);
 
     let currentTemp = document.createElement("p");
-    currentTemp.textContent = data.current.temp;
+    currentTemp.textContent = "Temp: " + data.current.temp + "°F";
 
     let currentWind = document.createElement("p");
-    currentWind.textContent = data.current.wind_speed;
+    currentWind.textContent = "Wind: " + data.current.wind_speed + "mph";
 
     let currentHumidity = document.createElement("p");
-    currentHumidity.textContent = data.current.humidity;
+    currentHumidity.textContent = "Humidity: " + data.current.humidity + "%";
 
     let currentUV = document.createElement("p");
-    currentUV.textContent = data.current.uvi;
+    let uvSpan = document.createElement("span")
+    uvSpan.innerHTML = data.current.uvi
+    currentUV.textContent = "UV Index: ";
+    currentUV.append(uvSpan);
     if (data.current.uvi <= 2) {
-        currentUV.setAttribute("class", "success")
+        uvSpan.setAttribute("class", "success")
     } else if (data.current.uvi > 2 && data.current.uvi < 8) {
-        currentUV.setAttribute("class", "warning");
+        uvSpan.setAttribute("class", "warning");
     } else if (data.current.uvi === 8) {
-        currentUV.setAttribute("class", "danger");
+        uvSpan.setAttribute("class", "danger");
     }
 
     jumboEl.append(cityName, currentTemp, currentWind, currentHumidity, currentUV);
@@ -76,15 +78,22 @@ function getDailyWeather(data) {
     weatherCard.setAttribute("class", "justify-content-evenly row")
     
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 1; i < 6; i++) {
         let date = new Date((data.daily[i].dt*1000)-(data.timezone_offset*1000))
         let dateCard = document.createElement("div")
-        dateCard.setAttribute("class", "card col-2")
+        dateCard.setAttribute("class", "card col-2 lower-cards")
         let dateEl = document.createElement("h4")
         console.log((date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear())
         dateEl.textContent = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear()
 
+        let dailyIconEl = document.createElement("img");
+        let icon = data.daily[i].weather[0].icon
+        dailyIconEl.setAttribute("src", "http://openweathermap.org/img/wn/"+ icon + "@2x.png");
+        dailyIconEl.setAttribute("class", "weather-icon");
+        dateEl.append(dailyIconEl)
+
         let dailyTemp = document.createElement("p");
+        dailyTemp.setAttribute("class", "card-p")
         dailyTemp.textContent = data.daily[i].temp.max
 
         let dailyWind = document.createElement("p");
@@ -101,4 +110,15 @@ function getDailyWeather(data) {
 
 }
 
-cityFormEl.addEventListener("submit", getLonLat)
+function formSubmitHandler(event) {
+    let elementExists = document.querySelector(".lower-cards")
+
+    if(elementExists) {
+        clearPage()
+        getLonLat(event)
+    } else {
+        getLonLat(event);
+    }
+}
+
+cityFormEl.addEventListener("submit", formSubmitHandler)
